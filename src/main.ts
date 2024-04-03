ca52276f8baba6035d1810b749eea61d9c3b0293
song_woojin <ujins8201@gmail.com>
2024-04-03 21:32:56 +0900

    1st CoMMIt

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT;
+  await app.listen(port);
+  console.log(`---------- SERVER_START_ON_PORT ${port} ----------`);
+}
+bootstrap();
