7632ba91279408ad9ec1e46bcf715db6165f7645
song_woojin <ujins8201@gmail.com>
2024-04-03 22:07:59 +0900

    FEAT: ADD WINSTON_LOGGER

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { winstonLogger } from './common/utils/winston.util';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: winstonLogger,
+  });
   const port = process.env.PORT;
   await app.listen(port);
   console.log(`---------- SERVER_START_ON_PORT ${port} ----------`);
